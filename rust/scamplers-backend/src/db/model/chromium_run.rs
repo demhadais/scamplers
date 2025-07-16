7dc415e9e56ffb2f58a52e2e14f5567c129594cb
Ahmed Said <ahmed.said@jax.org>
2025-07-16 13:22:14 -0400

    dummy commit

--- rust/scamplers-backend/src/db/model/chromium_run.rs
+++ rust/scamplers-backend/src/db/model/chromium_run.rs
@@ -1,1 +1,83 @@
+use scamplers_core::model::{
+    chromium_run::{
+        ChromiumRun, NewChipLoadingCommon, NewChromiumRun, NewMultiplexGems,
+        NewSingleplexChipLoading, NewSingleplexGems,
+    },
+    suspension::{MeasurementDataCore, SuspensionMeasurement},
+};
+use uuid::Uuid;
 
+use crate::db::model::WriteToDb;
+
+trait MeasurementDataCoreExt {
+    fn validate_volume(&self) -> crate::db::error::Result<()>;
+}
+
+impl MeasurementDataCoreExt for MeasurementDataCore {
+    fn validate_volume(&self) -> crate::db::error::Result<()> {
+        if !matches!(self, Self::Volume { .. }) {
+            return Err(crate::db::error::Error::Other {
+                message: "invalid chip loading volume".to_string(),
+            });
+        }
+
+        Ok(())
+    }
+}
+
+trait NewChipLoadingCommonExt {
+    fn validate_volumes(&self) -> crate::db::error::Result<()>;
+}
+
+impl NewChipLoadingCommonExt for NewChipLoadingCommon {
+    fn validate_volumes(&self) -> crate::db::error::Result<()> {
+        let volumes = [&self.suspension_volume_loaded, &self.buffer_volume_loaded];
+
+        Ok(())
+    }
+}
+
+enum NewGemsWrapper<'a> {
+    Singleplex(&'a [NewSingleplexGems]),
+    Multiplex(&'a [NewMultiplexGems]),
+}
+
+impl NewGemsWrapper {
+    fn validate_volumes(&self) -> crate::db::error::Result<()> {
+        let (suspension_volume, buffer_volume) = match self {
+            Self::Singleplex(NewSingleplexGems {
+                loading: NewSingleplexChipLoading { inner, .. },
+                ..
+            }) => [inner.suspension_volume_loaded, inner.buffer_volume_loaded],
+        };
+    }
+}
+
+trait NewChromiumRunExt {
+    fn gems_chip_loading(&mut self, self_id: Uuid) -> NewGemsWrapper;
+}
+
+impl NewChromiumRunExt for NewChromiumRun {
+    fn gems_chip_loading(&mut self, self_id: Uuid) -> NewGemsWrapper {
+        let gems = match self {
+            Self::Singleplex(singleplex) => singleplex.gems,
+        };
+    }
+}
+
+impl WriteToDb for NewChromiumRun {
+    type Returns = ChromiumRun;
+
+    async fn write_to_db(
+        self,
+        db_conn: &mut diesel_async::AsyncPgConnection,
+    ) -> crate::db::error::Result<Self::Returns> {
+        match self {
+            NewChromiumRun::Singleplex(singleplex) => todo!(),
+            NewChromiumRun::Ocm(ocm) => todo!(),
+            NewChromiumRun::PoolMultiplex(multiplex) => todo!(),
+        };
+
+        Ok(todo!())
+    }
+}
--- rust/scamplers-core/src/model/chromium_run.rs
+++ rust/scamplers-core/src/model/chromium_run.rs
@@ -1,7 +1,21 @@
-use scamplers_macros::base_api_model;
+use scamplers_macros::{base_api_model, db_selection};
+use time::OffsetDateTime;
+use uuid::Uuid;
 
-use crate::model::chromium_run::{
-    mutiplex::NewMultiplexChromiumRun, ocm::NewOcmChromiumRun, singleplex::NewSingleplexChromiumRun,
+use crate::model::{
+    chromium_run::{
+        mutiplex::NewMultiplexChromiumRun, ocm::NewOcmChromiumRun,
+        singleplex::NewSingleplexChromiumRun,
+    },
+    person::PersonHandle,
+};
+#[cfg(feature = "backend")]
+use scamplers_schema::{chromium_run, gems};
+pub use {
+    common::NewChipLoadingCommon,
+    mutiplex::{NewMultiplexChipLoading, NewMultiplexGems},
+    ocm::NewOcmGems,
+    singleplex::{NewSingleplexChipLoading, NewSingleplexGems},
 };
 
 mod common;
@@ -15,4 +29,49 @@ pub enum NewChromiumRun {
     Singleplex(#[garde(dive)] NewSingleplexChromiumRun),
     Ocm(#[garde(dive)] NewOcmChromiumRun),
     PoolMultiplex(#[garde(dive)] NewMultiplexChromiumRun),
+}
+
+#[db_selection]
+#[cfg_attr(feature = "backend", diesel(table_name = chromium_run))]
+pub struct ChromiumRunHandle {
+    pub id: Uuid,
+    pub link: String,
+}
+
+#[db_selection]
+#[cfg_attr(feature = "backend", diesel(table_name = chromium_run))]
+pub struct ChromiumRunSummary {
+    #[serde(flatten)]
+    #[cfg_attr(feature = "backend", diesel(embed))]
+    pub handle: ChromiumRunHandle,
+    pub readable_id: String,
+    pub chip: String,
+    pub run_at: OffsetDateTime,
+    pub succeeded: bool,
+    pub notes: Option<String>,
+}
+
+#[db_selection]
+#[cfg_attr(feature = "backend", diesel(table_name = chromium_run))]
+pub struct ChromiumRunCore {
+    #[serde(flatten)]
+    #[cfg_attr(feature = "backend", diesel(embed))]
+    pub summary: ChromiumRunSummary,
+    #[cfg_attr(feature = "backend", diesel(embed))]
+    pub run_by: PersonHandle,
+}
+
+#[db_selection]
+#[cfg_attr(feature = "backend", diesel(table_name = gems))]
+pub struct GemsHandle {
+    pub id: Uuid,
+    pub link: String,
+}
+
+#[base_api_model]
+#[cfg_attr(feature = "python", pyo3::pyclass)]
+pub struct ChromiumRun {
+    #[serde(flatten)]
+    pub core: ChromiumRunCore,
+    pub gems: Vec<GemsHandle>,
 }
