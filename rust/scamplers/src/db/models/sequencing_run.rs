cb999cec4a0e9bbb558a4a453a8ae060f5bfa4cb
Ahmed Said <ahmed.said@jax.org>
2025-08-20 18:21:25 -0400

    Update sequencing_run.rs
    Add sequencing submission

--- rust/scamplers/src/db/models/sequencing_run.rs
+++ rust/scamplers/src/db/models/sequencing_run.rs
@@ -1,1 +1,33 @@
+#[cfg(feature = "python")]
+use pyo3::prelude::*;
+use scamplers_macros::{db_insertion};
+use time::OffsetDateTime;
+use uuid::Uuid;
+use valid_string::ValidString;
 
+#[db_insertion]
+pub struct NewSequencingSubmission {
+    #[serde(default)]
+    pub sequencing_run_id: Uuid,
+    pub library_id: Uuid,
+    pub submitted_at: OffsetDateTime,
+}
+
+#[cfg(feature = "python")]
+#[pyo3_stub_gen::derive::gen_stub_pyclass]
+#[pymethods]
+impl NewSequencingSubmission {
+    #[new]
+    #[pyo3(signature = (*, library_id, submitted_at, sequencing_run_id=Uuid::default()))]
+    fn new(
+        library_id: Uuid,
+        submitted_at: OffsetDateTime,
+        sequencing_run_id: Uuid,
+    ) -> Self {
+        Self {
+            sequencing_run_id,
+            library_id,
+            submitted_at,
+        }
+    }
+}
