daa01cc9be2851350f08599c91413b1a30d6c1a4
Ahmed Said <ahmed.said@jax.org>
2025-10-22 12:15:44 -0400

    fix vitest config (#290)
    * fix vitest config
    
    * fix vitest config

--- typescript/scamplers-frontend/vite.config.ts
+++ typescript/scamplers-frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
 import { svelteTesting } from '@testing-library/svelte/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import wasm from 'vite-plugin-wasm';
 
 export default defineConfig({
